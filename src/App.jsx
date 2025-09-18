b2d9eb3014fdd5ca99c4d2b3198ab7ecd68d88e2
Bryan7905 <galangbryan99@gmail.com>
2025-09-18 15:46:46 +0800

    Second Commit: Applied the components and props. Added name, subject, course, year and section.

--- src/App.jsx
+++ src/App.jsx
@@ -3,36 +3,76 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+function Header() {
+    return <div class="header">
+        <Header_content/>
+        <Header_content2/>
+        <Header_content/>
+    </div>
+}
+
+function Header_content(){
+    return <div class="header-content"></div>
+}
+
+function Header_content2(){
+    return <div class="header-content2">
+        <Header_content2_center name="Bryan"/>
+        <Header_content2_center name="Galang"/>
+    </div>
+}
+
+function Header_content2_center(props){
+    return <div class="header-content2-center">{props.name}</div>
+}
+
+function Main(){
+    return <div class="main">
+        <Main_container/>
+        <Main_container2/>
+    </div>
+}
+
+function Main_container(){
+    return <div class="main-container"></div>
+}
+
+function Main_container2(){
+    return <div class="main-container2">
+        <Main_container_content2/>
+        <Main_container_content2/>
+    </div>
+}
+
+function Main_container_content2(){
+    return <div class="main-container-content2"></div>
+}
+
+function Footer_content(){
+    return <div class="footer-content"></div>
+}
+
+function Footer_content2(props){
+    return <div class="footer-content2">{props.text}</div>
+}
+
+function Footer(){
+    return <div class="footer">
+        <Footer_content/>
+        <Footer_content2 text="C-PCIT9"/>
+        <Footer_content2 text="IT3A"/>
+        <Footer_content/>
+    </div>
+}
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div class="container">
-        <div class="header">
-            <div class="header-left"></div>
-            <div class="header-center">
-                <div class="header-center-left"></div>
-                <div class="header-center-right"></div>
-            </div>
-            <div class="header-right"></div>  
-        </div>
-        
-        <div class="main">
-            <div class="left">
-                <div class="full-column"></div>
-            </div>
-            <div class="right">
-                <div class="upper"></div>
-                <div class="lower"></div>  
-            </div>
-        </div>
-        
-        <div class="footer">
-            <div class="footer-blue"></div>
-            <div class="footer-pink"></div>
-            <div class="footer-pink"></div>
-            <div class="footer-blue"></div>    
-        </div>
+        <Header/>
+        <Main/>
+        <Footer/>
     </div>
   )
 }
